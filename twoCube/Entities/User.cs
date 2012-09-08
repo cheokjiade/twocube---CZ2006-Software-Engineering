5db43d4e7b15eb7fb0b22f72d425be2096e77f02
cheokjiade <cheokjiade@gmail.com>
2012-09-08 19:13:15 +0800

    added a mapping from user to survey

--- twoCube/Entities/User.cs
+++ twoCube/Entities/User.cs
@@ -7,5 +7,19 @@ namespace twoCube.Entities
 {
     public class User
     {
+        public virtual int Id { get; set; }
+        public virtual string userName { get; set; }
+        public virtual string userPassword { get; set; }
+        public virtual IList<Survey> userSurveys { get; set; }
+
+        public User() 
+        {
+            userSurveys = new List<Survey>();
+        }
+
+        public virtual void AddSurvey(Survey survey)
+        {
+            userSurveys.Add(survey);
+        }
     }
 }
