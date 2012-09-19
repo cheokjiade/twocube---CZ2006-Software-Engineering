c4456eaa88a6631842083d34fd711aa935c2c8cc
June <jquak1@gmail.com>
2012-09-19 15:22:40 +0800

    Added new use case -> Update password, edited registration

--- twoCube/Services/MemberControllerService.asmx.cs
+++ twoCube/Services/MemberControllerService.asmx.cs
@@ -7,6 +7,7 @@ using System.Web.Script.Services;
 using System.Web.Script.Serialization;
 using System.IO;
 using twoCube.Entities;
+using Newtonsoft.Json.Linq;
 
 namespace twoCube.Services
 {
@@ -21,18 +22,27 @@ namespace twoCube.Services
     public class MemberControllerService : System.Web.Services.WebService
     {
 
-        [WebMethod(Description = "Add User")]
+        [WebMethod(Description = "Add User. String firstName, String lastName, String userName, Stirng password, int age, String location, String email, String qn, String answer")]
         [ScriptMethod(ResponseFormat = ResponseFormat.Json, UseHttpGet = true)]
-        public void AddUser(int id, string firstName, string lastName, string userName, string password, int age, string loc, string email, 
-            string qn, string ans)
+        public void AddUser(string jsonString)
         {
             using (var session = FluentNHibernateConfiguration.InitFactory.sessionFactory.OpenSession())
             {
                 using (var transaction = session.BeginTransaction())
                 {
-                    var user = new Entities.Member { Id = id, memberFirstName= firstName, memberLastName= lastName, userName = userName, 
-                    memberPassword = password, memberAge = age, memberLocation = loc, memberEmail = email, memberQuestion = qn, 
-                    memberAnswer = ans};
+                    JObject jsonObject = JObject.Parse(jsonString);
+                    var user = new Entities.Member
+                    {
+                        memberFirstName = jsonObject.SelectToken("firstName").ToString(),
+                        memberLastName = jsonObject.SelectToken("lastName").ToString(),
+                        userName = jsonObject.SelectToken("userName").ToString(),
+                        memberPassword = jsonObject.SelectToken("password").ToString(),
+                        memberAge = Int32.Parse(jsonObject.SelectToken("age").ToString()),
+                        memberLocation = jsonObject.SelectToken("location").ToString(),
+                        memberEmail = jsonObject.SelectToken("email").ToString(),
+                        memberQuestion = jsonObject.SelectToken("sqn").ToString(),
+                        memberAnswer = jsonObject.SelectToken("sans").ToString()
+                    };
 
                     session.Save(user);
                     transaction.Commit();
@@ -40,6 +50,50 @@ namespace twoCube.Services
                     JavaScriptSerializer js = new JavaScriptSerializer();
                     Context.Response.Write(js.Serialize(user));
                 }
+            }
+        }
+
+        [WebMethod(Description = "View User. Take in string username, string password")]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json, UseHttpGet = true)]
+        public void ViewUserDetails(string jsonString)
+        {
+            using (var session = FluentNHibernateConfiguration.InitFactory.sessionFactory.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                { }
+            }
+        }
+
+        [WebMethod(Description = "Update User, String firstName, String lastName, String userName, Stirng password, int age, String location, String email, String qn, String answer")]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json, UseHttpGet = true)]
+        public void UpdateUserDetails(string jsonString)
+        {
+            using (var session = FluentNHibernateConfiguration.InitFactory.sessionFactory.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                { }
+            }
+        }
+
+        [WebMethod(Description = "Update password, String userName, Stirng password")]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json, UseHttpGet = true)]
+        public void UpdateUserDetails(string jsonString)
+        {
+            using (var session = FluentNHibernateConfiguration.InitFactory.sessionFactory.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                { }
+            }
+        }
+
+        [WebMethod(Description = "Login, string username, string password")]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json, UseHttpGet = true)]
+        public void Login(string jsonString)
+        {
+            using (var session = FluentNHibernateConfiguration.InitFactory.sessionFactory.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                { }
             }
         }
 
