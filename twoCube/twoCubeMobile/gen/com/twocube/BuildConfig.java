fbc38a2422e2b4658dbdcb1bb370e06b98569eca
cheokjiade <cheokjiade@gmail.com>
2012-09-26 21:02:04 +0800

    Added android project for mobile survey #22

---
+++ twoCube/twoCubeMobile/gen/com/twocube/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.twocube;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ twoCube/twoCubeMobile/src/com/twocube/entities/SurveyQuestionOption.java
@@ -0,0 +1,5 @@
+package com.twocube.entities;
+
+public class SurveyQuestionOption {
+
+}
