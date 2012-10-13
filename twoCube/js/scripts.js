446841f2524463d781925c92d770ca3c466d3fa3
cheokjiade <cheokjiade@gmail.com>
2012-10-13 13:02:02 +0800

    Merged hong jing template

---
+++ twoCube/js/scripts.js
@@ -0,0 +1,18 @@
+function onload(){
+	loadMenu();
+	loadFooter();
+}
+function loadMenu(){
+	var menu = "<ul>\
+                <li><a href='index.html'>Home</a></li>\
+                <li><a href='index.html'>Create Survey</a></li>\
+                <li><a href='#'>About twoCube</a></li>\
+				<li><a href='#'>Login</a></li>\
+            </ul>\
+            <br style='clear: left' />";
+			
+	document.getElementById('menu').innerHTML = menu;
+}
+function loadFooter(){
+	document.getElementById('footer').innerHTML = "Copyright © 2013 twoCube<div class='cleaner'></div>";
+}
