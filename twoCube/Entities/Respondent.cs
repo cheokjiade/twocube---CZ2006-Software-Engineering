f9d85fab949f6c7f1efac58151e982087de6834e
cheokjiade <cheokjiade@gmail.com>
2012-10-26 13:56:30 +0800

    edited data logging and fb login

--- twoCube/Entities/Respondent.cs
+++ twoCube/Entities/Respondent.cs
@@ -9,6 +9,11 @@ namespace twoCube.Entities
     {
         public virtual int Id { get; set; }
         public virtual int respondentTime { get; set; }
+        public virtual DateTime respondentDateTime { get; set; }
+        public virtual string respondentCountryCode { get; set; }
+        public virtual string respondentBrowser { get; set; }
+        public virtual string respondentOS { get; set; }
+        public virtual string respondentLang { get; set; }
         public virtual string respondentIPAddress { get; set; }
         public virtual string respondentSessionID { get; set; }
         public virtual IList<SurveyQuestionResponse> surveyQuestionResponseList { get; set; }
