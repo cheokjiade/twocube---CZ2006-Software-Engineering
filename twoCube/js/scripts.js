8a03f0fe4ee30624e0313fdbbb2536e725a31ab4
Wesley <wesley.spisc@gmail.com>
2012-10-27 20:10:55 +0800

    Test 7

--- twoCube/js/scripts.js
+++ twoCube/js/scripts.js
@@ -7,6 +7,15 @@ $(document).bind('cbox_closed', function () {
     if (data != 0) $(location).attr('href', './');
 });
 
+// Load the SDK Asynchronously
+(function (d) {
+    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement('script'); js.id = id; js.async = true;
+    js.src = "//connect.facebook.net/en_US/all.js";
+    ref.parentNode.insertBefore(js, ref);
+} (document));
+
 function onload() {
     loadMenu();
     loadFooter();
